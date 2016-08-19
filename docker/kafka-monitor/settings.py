cd8335f80c46b8c147fd26782a8156c397d101fc
Madison Bahmer <madison.bahmer@istresearch.com>
2016-08-19 14:46:21 -0400

    Kafka Monitor Docker
    Adds a dockerfile to setup and run the kafka monitor. I do note that there are settings and configuration within that tie it into the docker-compose file, but I think they can be fairly easily overridden and maybe I switch them to just using the raw `settings.py` file instead of `localsettings.py`.
    
    This uses stand alone containers for Kafka, Zookeeper, and Redis, and each of the smaller scrapy cluster components
    will be their own separate containers as well.
    
    Everything will run under supervisord so you can scale the number of processes both within the container and outside. There is much more work to be done, for example reading in environment variables to override things in the settings file, configuring docker hub, etc.

---
+++ docker/kafka-monitor/settings.py
@@ -0,0 +1,53 @@
+# This file is a copy of
+
+# Redis host information
+REDIS_HOST = 'redis'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+# Kafka server information
+KAFKA_HOSTS = ['kafka:9092']
+KAFKA_INCOMING_TOPIC = 'demo.incoming'
+KAFKA_GROUP = 'demo-group'
+KAFKA_FEED_TIMEOUT = 10
+KAFKA_CONSUMER_AUTO_OFFSET_RESET = 'earliest'
+KAFKA_CONSUMER_TIMEOUT = 50
+KAFKA_CONSUMER_COMMIT_INTERVAL_MS = 5000
+KAFKA_CONSUMER_AUTO_COMMIT_ENABLE = True
+KAFKA_CONSUMER_FETCH_MESSAGE_MAX_BYTES = 10 * 1024 * 1024  # 10MB
+KAFKA_PRODUCER_BATCH_LINGER_MS = 25  # 25 ms before flush
+KAFKA_PRODUCER_BUFFER_BYTES = 4 * 1024 * 1024  # 4MB before blocking
+
+# plugin setup
+PLUGIN_DIR = 'plugins/'
+PLUGINS = {
+    'plugins.scraper_handler.ScraperHandler': 100,
+    'plugins.action_handler.ActionHandler': 200,
+    'plugins.stats_handler.StatsHandler': 300,
+    'plugins.zookeeper_handler.ZookeeperHandler': 400,
+}
+
+# logging setup
+LOGGER_NAME = 'kafka-monitor'
+LOG_DIR = 'logs'
+LOG_FILE = 'kafka_monitor.log'
+LOG_MAX_BYTES = 10 * 1024 * 1024
+LOG_BACKUPS = 5
+LOG_STDOUT = True
+LOG_JSON = False
+LOG_LEVEL = 'INFO'
+
+# stats setup
+STATS_TOTAL = True
+STATS_PLUGINS = True
+STATS_CYCLE = 5
+STATS_DUMP = 60
+# from time variables in scutils.stats_collector class
+STATS_TIMES = [
+    'SECONDS_15_MINUTE',
+    'SECONDS_1_HOUR',
+    'SECONDS_6_HOUR',
+    'SECONDS_12_HOUR',
+    'SECONDS_1_DAY',
+    'SECONDS_1_WEEK',
+]
