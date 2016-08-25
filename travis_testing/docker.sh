a0f3a9a7be9e5a8b09ba7d1a2a7c490493e3eca4
Madison Bahmer <madison.bahmer@istresearch.com>
2016-08-25 14:18:52 -0400

    Travis build matrix testing
    Trying to make life easier by separating things into shell scripts instead of a massive script section in travis

---
+++ travis_testing/docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# Build Dockerfiles for Scrapy Cluster
+sudo docker build --rm=true --file docker/kafka-monitor/Dockerfile --tag=istresearch/scrapy-cluster:kafka-monitor-test .
+sudo docker build --rm=true --file docker/redis-monitor/Dockerfile --tag=istresearch/scrapy-cluster:redis-monitor-test .
+sudo docker build --rm=true --file docker/crawler/Dockerfile --tag=istresearch/scrapy-cluster:crawler-test .
+
+# run docker compose up for docker tests
+sudo docker-compose -f docker-compose.test.yml up -d
+
+# run docker unit and integration tests for each component
+sudo docker exec -it scrapycluster_kafka_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+sudo docker exec -it scrapycluster_redis_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+sudo docker exec -it scrapycluster_crawler_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+
+# spin down compose
+sudo docker-compose down
