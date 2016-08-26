c8ec51d099c29a037fb1bf6bc061d9a7d0889c4e
Madison Bahmer <madison.bahmer@istresearch.com>
2016-08-26 10:01:05 -0400

    travis name change with file move

--- travis/docker.sh
+++ travis/docker.sh
@@ -11,13 +11,15 @@ sudo docker build --rm=true --file docker/crawler/Dockerfile --tag=istresearch/s
 sudo docker-compose -f travis/docker-compose.test.yml up -d
 
 # cat kafka logs to check things are working
+sudo docker-compose ps
 sudo docker-compose logs kafka_monitor
 sudo docker-compose logs kafka
 
+
 # run docker unit and integration tests for each component
-sudo docker exec -it scrapycluster_kafka_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
-sudo docker exec -it scrapycluster_redis_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
-sudo docker exec -it scrapycluster_crawler_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+sudo docker exec -it travis_kafka_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+sudo docker exec -it travis_redis_monitor_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
+sudo docker exec -it travis_crawler_1 env TERM=xterm /bin/bash -c "./run_docker_tests.sh"
 
 # spin down compose
 sudo docker-compose down
